64cf9ed604483058fe187a2871fbf3ce281aa8ec
Julian RaShaun Gay <jrg180004@utdallas.edu>
2024-03-14 17:15:25 -0500

    Prepped Angel's scene & made state

---
+++ Project Kerbal Barnyard/Assets/Scripts/GameStates/GameTutorialState.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameTutorialState : State
+{
+    private GameFSM _stateMachine;
+    private GameController _controller;
+
+    public GameTutorialState(GameFSM stateMachine, GameController controller)
+    {
+        _stateMachine = stateMachine;
+        _controller = controller;
+    }
+
+    public override void Enter() {
+        base.Enter();
+
+        Debug.Log("STATE: Game Setup");
+
+        // Disables everything on the canvas
+        // Iterate through all child GameObjects
+        /*foreach (Transform child in _controller.UI.canvas.transform)
+        {
+            // Set each child GameObject to inactive
+            child.gameObject.SetActive(false);
+        }*/
+
+        _controller.UI.overlayCanvas.SetActive(false);
+        _controller.UI.launchCanvas.SetActive(false);
+        _controller.UI.partsShopCanvas.SetActive(false);
+
+        // Activate canva elems
+        // _controller.playerController.player.SetActive(false);
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        //check for tap input
+        if(Input.GetMouseButtonDown(0)) {
+            _stateMachine.ChangeState(_stateMachine.BuildState);
+        }
+    }
+
+    public override void Exit() {
+        base.Exit();
+    }
+}
